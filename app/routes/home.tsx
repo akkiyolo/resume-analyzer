c36f00dcfabf0334e57cef0fc3947af9fdc22d97
akkiyolo <akshatshukla069@gmail.com>
2025-08-26 18:54:20 +0530

    deleted welcome and created main

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your application</h1>
+        <h2></h2>
+      </div>
+
+    </section>
+
+  </main>
 }
